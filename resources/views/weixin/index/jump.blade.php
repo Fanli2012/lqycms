5e8b6d4ce5ffe6f484c7c5c4c8c0d3bf796c9730
ZLW-PC\Administrator <374861669@qq.com>
2017-09-29 17:48:20 +0800

    系统提示页面

--- resources/views/weixin/index/jump.blade.php
+++ resources/views/weixin/index/jump.blade.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>跳转提示</title>
-<style type="text/css">
-*{ padding: 0; margin: 0; }
-body{ background: #fff; font-family: '微软雅黑'; color: #333; font-size: 16px; }
-.system-message{padding: 24px 48px;margin:5% auto 0 auto;width:600px;}
-.system-message h1{ font-size: 100px; font-weight: normal; line-height: 120px; margin-bottom: 12px; }
-.system-message .jump{ padding-top: 10px}
-.system-message .jump a{ color: #333;}
-.system-message .success,.system-message .error{ line-height: 1.8em; font-size: 36px }
-.system-message .detail{ font-size: 12px; line-height: 20px; margin-top: 12px; display:none}
+<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<title>系统提示</title><meta content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0" name="viewport">
+<link href="<?php echo env('APP_URL'); ?>/css/weixin/style.css" type="text/css" rel="stylesheet">
+<meta name="keywords" content="系统提示"><meta name="description" content="系统提示"></head><body>
+<style>
+.successsystem{text-align:center;padding:30px 0}
+.successsystem img{width:100px;height:100px}
+.prompt_s{font-size:16px;color:#999999;text-align:center}
+.systemprompt{text-align:center;margin-top:30px}
+.systemprompt a{display:inline-block;width:40%;height:36px;line-height:36px;background:#f23030;text-align:center;color:white;border-radius:3px;margin:0 10px;font-size:16px;}
 </style>
-</head><body>
+<div class="classreturn loginsignup">
+    <div class="ds-in-bl return"><a href="javascript:history.back(-1);"><img src="<?php echo env('APP_URL'); ?>/images/weixin/return.png" alt="返回"></a></div>
+    <div class="ds-in-bl tit center"><span>系统提示</span></div>
+</div>
 <?php
 /** 参数说明 
  * $_REQUEST['message']; //成功提示
@@ -20,16 +21,24 @@ body{ background: #fff; font-family: '微软雅黑'; color: #333; font-size: 16p
  * $_REQUEST['time']; //几秒后跳转
  */
 ?>
-<div class="system-message">
-<?php if(isset($_REQUEST['message'])) { ?>
-<h1>:)</h1>
-<p class="success"><?php echo $_REQUEST['message']; ?></p>
-<?php }elseif(isset($_REQUEST['error'])){ ?>
-<h1>:(</h1>
-<p class="error"><?php echo $_REQUEST['error']; ?></p>
-<?php } ?>
-<p class="detail"></p>
-<p class="jump">页面自动 <a id="href" href="<?php if(isset($_REQUEST['url'])){ echo $_REQUEST['url']; } ?>">跳转</a> 等待时间： <b id="wait"><?php if(isset($_REQUEST['time'])){ echo $_REQUEST['time']; } ?></b></p>
+<div class="successsystem">
+    <?php if(isset($_REQUEST['message'])) {?>
+        <img src="<?php echo env('APP_URL'); ?>/images/weixin/icogantanhao.png">
+    <?php }else{ ?>
+        <img src="<?php echo env('APP_URL'); ?>/images/weixin/icogantanhao-sb.png">
+    <?php }?>
+</div>
+<p class="prompt_s">
+    <?php if(isset($_REQUEST['message'])) { ?>
+    <?php echo $_REQUEST['message']; ?>
+    <?php }elseif(isset($_REQUEST['error'])){ ?>
+    <?php echo $_REQUEST['error']; ?>
+    <?php } ?>
+    ，等待时间：<b id="wait"><?php if(isset($_REQUEST['time'])){ echo $_REQUEST['time']; } ?></b>
+</p>
+<div class="systemprompt">
+    <a href="<?php if(isset($_REQUEST['url'])){ echo $_REQUEST['url']; } ?>" id="href">返回上一页</a>
+    <a href="<?php echo route('weixin'); ?>">返回首页</a>
 </div>
 <script type="text/javascript">
 (function(){
