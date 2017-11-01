9a3b734912b62996d30ec1eda189d619e80ed3b3
林一峰 <1feng.0595@gmail.com>
2017-11-02 00:17:33 +0800

    确认订单

--- resources/views/weixin/cart/cartCheckout.blade.php
+++ resources/views/weixin/cart/cartCheckout.blade.php
@@ -3,10 +3,11 @@
 <link href="<?php echo env('APP_URL'); ?>/css/weixin/style.css" type="text/css" rel="stylesheet">
 <script type="text/javascript" src="<?php echo env('APP_URL'); ?>/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo env('APP_URL'); ?>/js/weixin/mobile.js"></script>
-<meta name="keywords" content="关键词"><meta name="description" content="描述"></head><body>
+<link href="<?php echo env('APP_URL'); ?>/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+<meta name="keywords" content="关键词"><meta name="description" content="描述"></head><body style="background-color:#f1f1f1;">
 <div class="classreturn loginsignup">
     <div class="ds-in-bl return"><a href="javascript:history.back(-1);"><img src="<?php echo env('APP_URL'); ?>/images/weixin/return.png" alt="返回"></a></div>
-    <div class="ds-in-bl tit center"><span>购物车</span></div>
+    <div class="ds-in-bl tit center"><span>确认订单</span></div>
     <div class="ds-in-bl nav_menu"><a href="javascript:void(0);"><img src="<?php echo env('APP_URL'); ?>/images/weixin/class1.png" alt="菜单"></a></div>
 </div>
 
@@ -22,99 +23,77 @@
     </div>
 </div>
 
-<div class="cart_list">
-    <!--商品列表-s-->
-    <?php if($list){foreach($list as $k=>$v){ ?>
-    <div class="sc_list" id="cart_list_<?php echo $v['id']; ?>">
-        <div class="radio-img">
-            <div class="radio fl ">
-                <!--商品勾选按钮-->
-                <span onclick="checkGoods(this)" class="che">
-                 <i>
-                     <input name="checkItem" type="checkbox" style="display:none;" data-goods-id="<?php echo $v['goods_id']; ?>" data-cart-id="<?php echo $v['id']; ?>">
-                 </i>
-                 </span>
-            </div>
-            <div class="shopimg fl">
-                <a href="<?php echo $v['goods_detail_url']; ?>">
-                    <img src="<?php echo $v['litpic']; ?>">
-                </a>
-            </div>
-        </div>
-        <div class="deleshow">
-            <div class="deletes">
-                <!--商品名-->
-                <p class="tit"><?php echo $v['title']; ?></p>
-                <!--删除按钮-->
-                <a href="javascript:void(0);" class="delescj deleteGoods" data-cart-id="<?php echo $v['id']; ?>"><img src="<?php echo env('APP_URL'); ?>/images/weixin/dele.png"></a>
-            </div>
-            <!--商品属性，规格-->
-            <p class="weight"></p>
-            <div class="prices">
-                <p class="sc_pri fl">
-                    <!--商品单价-->
-                    <span>￥</span><span id="goods_price<?php echo $v['goods_id']; ?>"><?php echo $v['final_price']; ?></span>
-                </p>
-                <!--加减数量-->
-                <div class="plus fr get_mp">
-                    <span class="mp_minous" onClick="change_goods_number(1,<?php echo $v['goods_id']; ?>)">-</span>
-                    <span class="mp_mp"><input name="goods_number<?php echo $v['goods_id']; ?>" type="text" id="goods_number<?php echo $v['goods_id']; ?>" value="<?php echo $v['goods_number']; ?>" onkeyup="this.value=this.value.replace(/[^\d]/g,'')" autocomplete="off" value="1" onblur="change_goods_number(2,<?php echo $v['goods_id']; ?>)" class="input-num"></span>
-                    <span class="mp_plus" onClick="change_goods_number(3,<?php echo $v['goods_id']; ?>)">+</span>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php }} ?>
-    <!--商品列表-e-->
-    
-    <!--提交栏-s-->
-    <div class="foohi foohiext">
-        <div class="payit ma-to-20 payallb">
-            <div class="radio fl">
-                <span class="che alltoggle checkFull" onclick="checkGoods(this)">
-                    <i></i>
-                </span>
-                <span class="all">全选</span>
-            </div>
-            <div class="fr">
-                <a href="javascript:void(0);" onclick="cart_submit();">去结算</a>
-            </div>
-            <div class="youbia">
-                <p><span class="pmo">总计：</span><span>￥</span><span id="total_fee">0</span></p>
-            </div>
-        </div>
-    </div>
-    <!--提交栏-e-->
+<a href="/v2/index.php?m=default&amp;c=flow&amp;a=consignee_list&amp;u=53657">
+<div class="checkout-addr">
+  <p class="title">范例 <span>15280767654</span></p>
+  <p class="des">福建厦门湖里区 软件园二期100号</p>
+  <i></i>
 </div>
+</a>
+<style>
+.checkout-addr{position: relative;/* border-top: 1px solid #e3e3e3;border-bottom: 1px solid #e3e3e3; */background: #fff;margin-top:10px;padding:10px;}
+.checkout-addr p{margin-right:20px;}.checkout-addr .title{font-size:18px;margin-bottom:5px;}.checkout-addr .des{color:#666;}
+.checkout-addr i{position: absolute;top: 50%;right:12px;margin-top:-6px;color:#bbb;display:inline-block;border-right:2px solid;border-bottom:2px solid;width:12px;height:12px;transform:rotate(-45deg);}
+</style>
+<ul class="goodslist">
+<?php if($list){foreach($list as $k=>$v){ ?>
+<li>
+	<img src="<?php echo $v['litpic']; ?>">
+	<p><b><?php echo $v['title']; ?></b><span>￥<?php echo $v['final_price']; ?><i>x<?php echo $v['goods_number']; ?></i></span></p>
+</li>
+<?php }} ?>
+</ul>
+<style>
+.goodslist{background-color:#fff;margin-top:10px;}
+.goodslist li{display:-webkit-box;margin:0 10px;padding:10px;border-bottom: 1px solid #f1f1f1;}
+.goodslist li img{margin-right:10px;display:block;width:78px;height:78px;border: 1px solid #e1e1e1;}
+.goodslist li p {display: block;-webkit-box-flex: 1;width: 100%;}
+.goodslist li p b {display:block;font-size:16px;font-weight:400;line-height: 28px;color:#333;}
+.goodslist li p span {color:#f23030;font-size:18px;display: block;padding-top:8px;}
+.goodslist li p i{color:#666;float:right;font-size:14px;}
+</style>
 
-<?php if(!$list){ ?>
-<!--购物车没有商品-start-->
-<div class="cart_list">
-    <div class="nonenothing">
-        <img src="<?php echo env('APP_URL'); ?>/images/weixin/nothing.png">
-        <p>购物车暂无商品</p>
-        <a href="<?php echo route('weixin'); ?>">去逛逛</a>
-    </div>
-</div>
-<br><br>
-<!--猜你喜欢-start-->
 <div class="floor">
-<div class="banner_headline">
-    <div class="tit">
-        <h4>猜你喜欢</h4>
-    </div>
+<ul class="fui-list mt10">
+    <a href="javascript:update_username();"><li>
+        <div class="ui-list-info">
+            <h4 class="ui-nowrap">支付方式</h4>
+            <div class="ui-txt-info">支付宝 &nbsp;</div>
+        </div>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+    </li></a>
+    <a href="javascript:update_username();"><li>
+        <div class="ui-list-info">
+            <h4 class="ui-nowrap">优惠券</h4>
+            <div class="ui-txt-info">请选择优惠券 &nbsp;</div>
+        </div>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+    </li></a>
+</ul></div>
+
+<div class="floor" style="background-color:#fff;margin-top:10px;padding:10px;">
+<div class="buy_note">
+    <div class="buy_note_tit"><span>备注</span></div>
+    <textarea name="message"></textarea>
 </div>
-<div class="likeshop">
-    <ul class="goods_list">
-    <?php if($like_goods_list){foreach($like_goods_list as $k=>$v){ ?>
-        <li><a href="<?php echo $v['goods_detail_url']; ?>"><img alt="<?php echo $v['title']; ?>" src="<?php echo $v['litpic']; ?>"><div class="goods_info"><p class="goods_tit"><?php echo $v['title']; ?></p><div class="goods_price">￥<b><?php echo $v['price']; ?></b></div></div></a></li>
-    <?php }} ?>
-    </ul>
+<div class="order_check_info">
+    <p>共1件商品</p>
+    <p>运费：¥0</p>
+    <p>满¥15.00减¥1.00</p>
+    <p>商品总价：¥99</span></p>
+    <p>应付款金额：<span class="red">¥<i id="totalamount">99</i></span></p>
 </div>
 </div>
-<!--购物车没有商品-end-->
-<?php } ?>
-<br><br>
+<style>
+.buy_note{margin:5px 0 15px 0;}
+.buy_note_tit{font-size:16px;margin-bottom:15px;}
+.buy_note textarea{display:block;font-size: 14px;min-height:100px;border:1px solid #e1e1e1;width: 100%;padding:10px;box-sizing: border-box;}
+.order_check_info p{text-align:right;line-height:22px;color: #666;font-size:14px;}
+.order_check_info p .red{color:#ff5500;font-size:18px;}
+</style>
+
+<div class="setting"><div class="close"><a href="<?php echo route('weixin_user_logout'); ?>" id="logout">提交</a></div></div>
+
 <script type="text/javascript" src="<?php echo env('APP_URL'); ?>/js/layer/mobile/layer.js"></script>
 <script>
 function cart_submit()
